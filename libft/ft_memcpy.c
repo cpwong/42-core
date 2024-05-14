4fa5b80046574b7258f9daf242491c611c1e927f
cpwong <wongchenpang@gmail.com>
2024-05-15 00:55:40 +0800

    Add ft_memcpy

--- libft/ft_memcpy.c
+++ libft/ft_memcpy.c
@@ -2,11 +2,14 @@
 
 void	*ft_memcpy(void *dst, const void *src, size_t n)
 {
-	// char	*ptr;
+	unsigned char		*ptr_dst;
+	const unsigned char *ptr_src; 
 
 	if (!dst || !src)
 		return (NULL);
-	while (n-- > 0);
-	// 	dst++ = src++;
-	return (dst);	
+	ptr_dst = (unsigned char *)dst;
+	ptr_src = (const unsigned char *)src;
+	while (n-- > 0)
+	 	*ptr_dst++ = *ptr_src++;
+	return (dst);
 };
