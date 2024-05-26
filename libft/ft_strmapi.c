f7d8c1be5d99036b19a12c88929ccc030817299a
CP Wong <wongchenpang@gmail.com>
2024-05-26 14:44:32 +0800

    Commit strmapi

---
+++ libft/ft_strmapi.c
@@ -0,0 +1,18 @@
+#include "libft.h"
+
+char	*ft_strmapi(char const *s, char (*f)(unsigned int, char))
+{
+	char	*str;
+	size_t	len;
+
+	if (!s || !f)
+		return (NULL);
+	len = ft_strlen(s);
+	str = (char *)malloc(sizeof(char) * (len + 1));
+	if (!str)
+		return (NULL);
+	str[len] = '\0';
+	while (len--)
+		str[len] = f(len, s[len]);
+	return (str);
+}
