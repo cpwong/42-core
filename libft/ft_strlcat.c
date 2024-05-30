6ab02e6daba10c6949ba4bdbfcc2aae50cfe7599
CP Wong <wongchenpang@gmail.com>
2024-05-30 17:11:54 +0800

    Fix strlcat

--- libft/ft_strlcat.c
+++ libft/ft_strlcat.c
@@ -6,32 +6,69 @@
 /*   By: chenwong <chenwong@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/18 15:33:58 by chenwong          #+#    #+#             */
-/*   Updated: 2024/05/29 18:27:37 by chenwong         ###   ########.fr       */
+/*   Updated: 2024/05/30 17:11:30 by chenwong         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
+#include <stdio.h>
 
-size_t	ft_strlcat(char *dst, const char *src, size_t dstsize)
+// size_t	ft_strlcat(char *dst, const char *src, size_t dstsize)
+// {
+// 	size_t	dst_len;
+// 	size_t	src_len;
+// 	size_t	copy_len;
+
+// 	dst_len = ft_strlen(dst);
+// 	src_len = ft_strlen(src);
+// 	// printf("\tdst_size: %zu, dst_len: %zu, src_len: %zu\n", dstsize, dst_len, src_len);
+// 	if (dstsize <= dst_len)
+// 		return (dstsize + src_len);
+// 	copy_len = dstsize - dst_len - 1;
+// 	// printf("\tcopy_len: %zu\n", copy_len);
+// 	if (src_len < copy_len)
+// 		dst = ft_memcpy(dst + dst_len, src, src_len + 1);
+// 	else
+// 	{
+// 		dst = ft_memcpy(dst + dst_len, src, copy_len);
+// 		dst[copy_len] = '\0';
+// 	}
+// 	return (dst_len + src_len);
+// }
+
+#include "libft.h"
+
+static size_t	str_len(char const *str)
 {
-	size_t	dst_len;
-	size_t	src_len;
+	size_t	i;
 
-	if (dstsize == 0)
-		return (src_len);
-	dst_len = ft_strlen(dst);
-	src_len = ft_strlen(src);
-	if (dstsize <= dst_len)
-		return (dst_len + src_len);
-	if (dstsize >= src_len + dst_len)
-	{
-		ft_memcpy(dst, src, src_len);
-		dst[dst_len + src_len] = '\0';
-	}
+	i = 0;
+	while (*(str + i))
+		i++;
+	return (i);
+}
+
+size_t	ft_strlcat(char *dest, const char *src, size_t n)
+{
+	size_t		dest_len;
+	size_t		total_len;
+	const char	*s;
+
+	if ((!dest || !src) && !n)
+		return (0);
+	s = src;
+	dest_len = 0;
+	while (*(dest + dest_len) && dest_len < n)
+		dest_len++;
+	if (dest_len < n)
+		total_len = dest_len + str_len(s);
 	else
+		return (n + str_len(s));
+	while (*s && (dest_len + 1) < n)
 	{
-		ft_memcpy(dst, src, dstsize - dst_len - 1);
-		dst[dstsize - 1] = '\0';
+		*(dest + dest_len) = *s++;
+		dest_len++;
 	}
-	return (dst_len + src_len);
+	*(dest + dest_len) = '\0';
+	return (total_len);
 }
--- libft_tests/test_ft_strlcat.c
+++ libft_tests/test_ft_strlcat.c
@@ -1,13 +1,14 @@
 #include "../libft/libft.h"
 #include <stdlib.h>
 #include <stdio.h>
+#include <string.h>
 
 int	main(void)
 {
 	// Test case 1: destination buffer size exactly fits the concatenated string
 	char dst1[12] = "hello";
 	char src1[] = " world";
-	printf("Test Case 1: dst = \"hello\", src = \" world\", dstsize = sizeof(dst1)\n");
+	printf("Test Case 1: dst = \"hello\", src = \" world\", dstsize = %zu\n", sizeof(dst1));
 	printf("Expected Output: dst = \"hello world\", len = 11\n");
 	printf("Before: dst = \"%s\"\n", dst1);
 	size_t len1 = ft_strlcat(dst1, src1, sizeof(dst1));
@@ -17,7 +18,7 @@ int	main(void)
 	char dst2[10] = "abc";
 	char src2[] = "defghijkl";
 	printf("Test Case 2: dst = \"abc\", src = \"defghijkl\", dstsize = %zu\n", sizeof(dst2));
-	printf("Expected Output: dst = \"abcdefghi\", len = 10\n");
+	printf("Expected Output: dst = \"abcdefghi\", len = 12\n");
 	printf("Before: dst = \"%s\"\n", dst2);
 	size_t len2 = ft_strlcat(dst2, src2, sizeof(dst2));
 	printf("After: dst = \"%s\", len = %zu\n\n", dst2, len2);
@@ -26,7 +27,7 @@ int	main(void)
 	char dst3[5] = "1234";
 	char src3[] = "5678";
 	printf("Test Case 3: dst = \"1234\", src = \"5678\", dstsize = sizeof(dst3)\n");
-	printf("Expected Output: dst = \"1234\", len = 4\n");
+	printf("Expected Output: dst = \"1234\", len = 8\n");
 	printf("Before: dst = \"%s\"\n", dst3);
 	size_t len3 = ft_strlcat(dst3, src3, sizeof(dst3));
 	printf("After: dst = \"%s\", len = %zu\n\n", dst3, len3);
@@ -60,4 +61,19 @@ int	main(void)
 	// printf("Before: dst = \"%s\"\n", customDst);
 	// size_t len6 = ft_strlcat(customDst, customSrc, customDstSize);
 	// printf("After: dst = \"%s\", len = %zu\n\n", customDst, len6);
+
+	// Test case 7: Custom
+	char *dst7;
+	if (!(dst7 = (char *)malloc(sizeof(*dst7) * 15)))
+		return (0);
+	memset(dst7, 0, 15);
+	memset(dst7, 'r', 6);
+	dst7[10] = 'a';
+	char src7[] = "lorem ipsum dolor sit amet";
+	printf("Test Case 7: Custom\n");
+	printf("Before: dst = \"%s\"\n", dst7);
+	size_t len7 = ft_strlcat(dst7, src7, 1);
+	printf("After: dst = \"%s\", len = %zu\n\n", dst7, len7);
+
+
 }
