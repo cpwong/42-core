8634ae078739cc6a090ffc3371fc731883f84f70
CP Wong <wongchenpang@gmail.com>
2024-05-31 04:11:49 +0800

    Fix itoa

--- libft/ft_itoa.c
+++ libft/ft_itoa.c
@@ -6,13 +6,46 @@
 /*   By: chenwong <chenwong@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/26 17:06:09 by chenwong          #+#    #+#             */
-/*   Updated: 2024/05/26 22:15:31 by chenwong         ###   ########.fr       */
+/*   Updated: 2024/05/31 04:10:59 by chenwong         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
+#include <stdio.h>
 
-char	*create_itoa_string(int n, int len, int neg)
+int	ft_numlen(int n)
+{
+	int	len;
+
+	len = 0;
+	if (n <= 0)
+	{
+		len++;
+		n *= -1;
+	}
+	while (n > 0)
+	{
+		n /= 10;
+		len++;
+	}
+	return (len);
+}
+
+char	*ft_check_limits(int n)
+{
+	char	*str;
+
+	str = NULL;
+	if (n == 0)
+		str = ft_strdup("0");
+	else if (n <= INT_MIN)
+		str = ft_strdup("-2147483648");
+	else if (n >= INT_MAX)
+		str = ft_strdup("2147483647");
+	return (str);
+}
+
+char	*ft_prep_str(int n, int len)
 {
 	char	*str;
 
@@ -20,41 +53,36 @@ char	*create_itoa_string(int n, int len, int neg)
 	if (!str)
 		return (NULL);
 	str[len] = '\0';
-	if (neg)
+	if (n < 0)
 		str[0] = '-';
-	while (len-- > neg)
-	{
-		str[len] = (n % 10) * (1 - 2 * neg) + '0';
-		n /= 10;
-	}
 	return (str);
 }
 
 char	*ft_itoa(int n)
 {
-	int		len;
-	int		neg;
-	int		tmp;
 	char	*str;
+	int		len;
 
-	len = 1;
-	neg = 0;
-	if (n < 0)
+	str = NULL;
+	if (n == 0)
+		str = ft_strdup("0");
+	else if (n <= INT_MIN)
+		str = ft_strdup("-2147483648");
+	else if (n >= INT_MAX)
+		str = ft_strdup("2147483647");
+	else
 	{
-		neg = 1;
-		len++;
-	}
-	tmp = n;
-	while (tmp > 0)
-	{
-		tmp /= 10;
-		len++;
-	}
-	str = create_itoa_string(n, len, neg);
-	while (str && len-- > neg)
-	{
-		str[len] = (n % 10) * (1 - 2 * neg) + '0';
-		n /= 10;
+		len = ft_numlen(n);
+		str = ft_prep_str(n, len);
+		if (!str)
+			return (NULL);
+		if (n < 0)
+			n *= -1;
+		while (len-- > 0 && n > 0)
+		{
+			str[len] = n % 10 + '0';
+			n /= 10;
+		}
 	}
 	return (str);
 }
