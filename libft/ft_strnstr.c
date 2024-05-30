d1739a0438c39ada31b5456a18bc45dda5546b51
CP Wong <wongchenpang@gmail.com>
2024-05-31 00:51:38 +0800

    Fix strnstr

--- libft/ft_strnstr.c
+++ libft/ft_strnstr.c
@@ -6,33 +6,37 @@
 /*   By: chenwong <chenwong@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/26 21:16:31 by chenwong          #+#    #+#             */
-/*   Updated: 2024/05/26 21:56:26 by chenwong         ###   ########.fr       */
+/*   Updated: 2024/05/31 00:50:54 by chenwong         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-char	*ft_strnstr(const char *str, const char *tofind, size_t len)
+char	*ft_strnstr(const char *str, const char *find, size_t len)
 {
-	const char	*first_ptr;
 	const char	*ptr;
+	const char	*find_ptr;
 
-	ptr = tofind;
-	first_ptr = str;
-	while (*str != '\0' && len-- > 0)
-	{
-		if (*ptr == '\0')
-			return ((char *)first_ptr);
-		if (*str != *ptr)
-		{
-			ptr = tofind;
-			first_ptr = str;
-		}
-		ptr++;
-		str++;
-	}
-	if (*ptr == '\0')
-		return ((char *)first_ptr);
-	else
+	if (str == NULL && len == 0)
 		return (NULL);
+	if (*find == '\0')
+		return ((char *)str);
+	find_ptr = find;
+	ptr = str;
+	while (*str && len-- > 0)
+	{
+		if (*find_ptr != *str)
+		{
+			find_ptr = find;
+			ptr = str + 1;
+		}
+		else
+		{
+			find_ptr++;
+		}
+		str++;
+		if (*find_ptr == '\0')
+			return ((char *)ptr);
+	}
+	return (NULL);
 }
--- libft_tests/test_ft_strnstr.c
+++ libft_tests/test_ft_strnstr.c
@@ -1,39 +1,69 @@
-#include "../libft.h"
+#include "../libft/libft.h"
 #include <stdio.h>
 #include <string.h>
 
 int main(void) {
 	char str1[] = "hello world";
 	char tofind1[] = "world";
-	printf("Test Case 1: str = \"hello world\", tofind = \"world\", len = ft_strlen(str1)\n");
+	printf("Test Case 1: str = \"hello world\", tofind = \"world\", len = %zu\n", ft_strlen(str1));
+	printf("str = %p, | %c tofind = %p | %c\n", str1, *str1, tofind1, *tofind1);
 	printf("Expected Output: A pointer to the substring \"world\" in \"hello world\"\n");
 	char *result1 = ft_strnstr(str1, tofind1, ft_strlen(str1));
+	printf("Result: %p\n", result1 != NULL ? result1 : "NULL");
 	printf("Result: %s\n\n", result1 != NULL ? result1 : "NULL");
 
 	char str2[] = "hello world";
 	char tofind2[] = "xyz";
-	printf("Test Case 2: str = \"hello world\", tofind = \"xyz\", len = ft_strlen(str2)\n");
+	printf("Test Case 2: str = \"hello world\", tofind = \"xyz\", len = %zu\n", ft_strlen(str2));
+	printf("str = %p\n", str2);
 	printf("Expected Output: NULL\n");
 	char *result2 = ft_strnstr(str2, tofind2, ft_strlen(str2));
+	printf("Result: %p\n", result2 != NULL ? result2 : "NULL");
 	printf("Result: %s\n\n", result2 != NULL ? result2 : "NULL");
 
 	char str3[] = "hello world";
 	char tofind3[] = "hello";
-	printf("Test Case 3: str = \"hello world\", tofind = \"hello\", len = 5\n");
+	size_t len3 = 5;
+	printf("Test Case 3: str = \"hello world\", tofind = \"hello\", len = %zu\n", len3);
+	printf("str = %p\n", str3);
 	printf("Expected Output: A pointer to the substring \"hello\" in \"hello world\"\n");
-	char *result3 = ft_strnstr(str3, tofind3, 5);
+	char *result3 = ft_strnstr(str3, tofind3, len3);
+	printf("Result: %p\n", result3 != NULL ? result3 : "NULL");
 	printf("Result: %s\n\n", result3 != NULL ? result3 : "NULL");
 
-	// Additional test cases (edge cases)
-	printf("Enter a string to search: ");
-	char customStr[100];
-	scanf(" %99[^\n]", customStr);
-	printf("Enter a substring to find: ");
-	char customTofind[100];
-	scanf(" %99[^\n]", customTofind);
-	printf("Enter the maximum number of bytes to search: ");
-	size_t customLen;
-	scanf("%zu", &customLen);
-	char *result4 = ft_strnstr(customStr, customTofind, customLen);
+	char str4[] = "aaxx";
+	char tofind4[] = "xx";
+	size_t len4 = 2;
+	printf("Test Case 4: str = %s, tofind = %s, len = %zu\n", str4, tofind4, len4);
+	printf("str = %p\n", str4);
+	char *result4 = ft_strnstr(str4, tofind4, len4);
+	printf("Result: %p\n", result4 != NULL ? result4 : "NULL");
 	printf("Result: %s\n\n", result4 != NULL ? result4 : "NULL");
+
+	size_t len5 = 3;
+	printf("Test Case 4: str = %s, tofind = %s, len = %zu\n", str4, tofind4, len5);
+	printf("str = %p, find = %p\n", str4, tofind4);
+	char *result5 = ft_strnstr(str4, tofind4, len5);
+	printf("Result: %p\n", result5 != NULL ? result5 : "NULL");
+	printf("Result: %s\n\n", result5 != NULL ? result5 : "NULL");
+
+	size_t len6 = 4;
+	printf("Test Case 4: str = %s, tofind = %s, len = %zu\n", str4, tofind4, len6);
+	printf("str = %p, find = %p\n", str4, tofind4);
+	char *result6 = ft_strnstr(str4, tofind4, len6);
+	printf("Result: %p\n", result6 != NULL ? result6 : "NULL");
+	printf("Result: %s\n\n", result6 != NULL ? result6 : "NULL");
+
+	// Additional test cases (edge cases)
+	// printf("Enter a string to search: ");
+	// char customStr[100];
+	// scanf(" %99[^\n]", customStr);
+	// printf("Enter a substring to find: ");
+	// char customTofind[100];
+	// scanf(" %99[^\n]", customTofind);
+	// printf("Enter the maximum number of bytes to search: ");
+	// size_t customLen;
+	// scanf("%zu", &customLen);
+	// char *result4 = ft_strnstr(customStr, customTofind, customLen);
+	// printf("Result: %s\n\n", result4 != NULL ? result4 : "NULL");
 }
